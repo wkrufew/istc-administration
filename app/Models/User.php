e3908cd3d8c8cbebb02b3e086ff3f6385c9b9f8d
Smith Aviles <42518427+wkrufew@users.noreply.github.com>
2026-05-18 09:47:43 -0500

    feat: softdelete usuarios, filtro roles, editar/eliminar en listado, papelera y reenvio credenciales

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -15,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 class User extends Authenticatable
 {
     use HasRoles;
+    use SoftDeletes;
 
     use HasApiTokens;
 
